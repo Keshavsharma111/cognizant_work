4a17c5220f69664f1d7b18dbeb933812a0e3eccc
keshav sharma <keshavsharma7180@gmail.com>
2026-07-14 14:14:29 +0530

    Week 3 5JWT Handson

--- WEEK3/Spring REST using Spring Boot 3/5JWT-handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
+++ WEEK3/Spring REST using Spring Boot 3/5JWT-handson/spring-learn/src/main/java/com/cognizant/springlearn/controller/AuthenticationController.java
@@ -1,4 +1,36 @@
 package com.cognizant.springlearn.controller;
 
+import com.cognizant.springlearn.model.JwtResponse;
+import com.cognizant.springlearn.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@RestController
 public class AuthenticationController {
+
+    @GetMapping("/authenticate")
+    public JwtResponse authenticate(HttpServletRequest request) {
+
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Basic ")) {
+            throw new RuntimeException("Authorization header missing");
+        }
+
+        String base64Credentials = authorizationHeader.substring(6);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+
+        String credentials = new String(decodedBytes, StandardCharsets.UTF_8);
+
+        String username = credentials.split(":")[0];
+
+        String token = JwtUtil.generateToken(username);
+
+        return new JwtResponse(token);
+    }
 }
--- WEEK3/Spring REST using Spring Boot 3/5JWT-handson/spring-learn/src/main/java/com/cognizant/springlearn/util/JwtUtil.java
+++ WEEK3/Spring REST using Spring Boot 3/5JWT-handson/spring-learn/src/main/java/com/cognizant/springlearn/util/JwtUtil.java
@@ -1,4 +1,29 @@
 package com.cognizant.springlearn.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
 public class JwtUtil {
+
+    // 32+ characters key required for HS256
+    private static final String SECRET =
+            "myspringsecretkeymyspringsecretkey123";
+
+    private static final SecretKey KEY =
+            Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+
+    public static String generateToken(String username) {
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
