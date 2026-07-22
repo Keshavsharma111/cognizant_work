adcb060329d0c67b43a1e3c7262c79ef5c482f0d
keshav sharma <keshavsharma7180@gmail.com>
2026-07-22 21:58:55 +0530

    WEEK5 ReactJS Hands-on 10

---
+++ WEEK5/React/10_ReactJS-HOL/officespacerentalapp/src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+import office from "./office.jpg";
+
+function App() {
+
+  const offices = [
+    {
+      Name: "DBS",
+      Rent: 50000,
+      Address: "Chennai",
+      Image: office
+    },
+    {
+      Name: "Regus",
+      Rent: 75000,
+      Address: "Bangalore",
+      Image: office
+    },
+    {
+      Name: "SmartWorks",
+      Rent: 55000,
+      Address: "Hyderabad",
+      Image: office
+    }
+  ];
+
+  return (
+    <div className="App">
+
+      <h1>Office Space, at Affordable Range</h1>
+
+      {
+        offices.map((item,index)=>{
+
+          const color={
+            color:item.Rent<=60000?"red":"green"
+          };
+
+          return(
+
+            <div key={index} className="card">
+
+              <img
+                src={item.Image}
+                alt="Office Space"
+                width="300"
+                height="200"
+              />
+
+              <h2>Name: {item.Name}</h2>
+
+              <h3 style={color}>
+                Rent: Rs. {item.Rent}
+              </h3>
+
+              <h3>
+                Address: {item.Address}
+              </h3>
+
+            </div>
+
+          );
+
+        })
+      }
+
+    </div>
+  );
+}
+
+export default App;
